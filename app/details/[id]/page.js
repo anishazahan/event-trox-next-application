80d737a361ed9a7bf19d3081ffdeada34843aa3e
anishazahan <anishazahan13@gmail.com>
2024-09-14 08:01:35 +0600

    get all events from db and show events and details

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -1,19 +1,22 @@
+import { getEventById } from "@/app/Backend/Queries/queries";
 import EventDetails from "@/app/Components/Details/EventDetails";
 import EventVenue from "@/app/Components/Details/EventVenue";
 import HeroSection from "@/app/Components/Details/HeroSection";
 
-const page = () => {
+const EventDetailsPage = async ({ params: { id } }) => {
+  const eventInfo = await getEventById(id);
+
   return (
     <>
-      <HeroSection />
+      <HeroSection eventInfo={eventInfo} />
       <section class="container mx-auto">
-        <div class="grid grid-cols-5 gap-12 my-12 mx-auto">
-          <EventDetails />
-          <EventVenue />
+        <div class="grid grid-cols-5 gap-12 my-12">
+          <EventDetails details={eventInfo?.details} swags={eventInfo?.swags} />
+          <EventVenue location={eventInfo?.location} />
         </div>
       </section>
     </>
   );
 };
 
-export default page;
+export default EventDetailsPage;
