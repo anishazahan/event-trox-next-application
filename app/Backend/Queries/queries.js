1f8bfc1d5ca26897a09c33d357eae40d52e50283
anishazahan <anishazahan13@gmail.com>
2024-09-14 07:26:50 +0600

    mongodb connected

---
+++ app/Backend/Queries/queries.js
@@ -0,0 +1,14 @@
+import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/app/utils/data-util";
+import { eventModel } from "../Models/event.models";
+
+async function getAllEvents() {
+  const allEvents = await eventModel.find().lean();
+  return replaceMongoIdInArray(allEvents);
+}
+
+async function getEventById(eventId) {
+  const event = await eventModel.findById(eventId).lean();
+  return replaceMongoIdInObject(event);
+}
+
+export { getAllEvents, getEventById };
